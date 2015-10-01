6dbaa15dabd4a61b70e4442ba8532496d2111073
foamliu <foamliu@yeah.net>
2015-10-01 14:25:11 +0800

    上一个commit缺了个文件。

---
+++ FiatCoinNet.Interface/IFiatCoinRepository.cs
@@ -0,0 +1,24 @@
+﻿using FiatCoinNet.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiatCoinNet.Interface
+{
+    public interface IFiatCoinRepository
+    {
+        PaymentAccount AddAccount(PaymentAccount newAccount);
+
+        PaymentAccount GetAccount(string address);
+
+        List<PaymentAccount> GetAccounts(int issuerId);
+
+        void CloseAccount(string address);
+
+        PaymentTransaction AddTransaction(PaymentTransaction newTransaction);
+
+        List<PaymentTransaction> GetTransactions(string address);
+    }
+}
