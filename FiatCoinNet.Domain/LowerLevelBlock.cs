295e8e984999b3dfce77f4d30c9d2693c07de197
luckyyd <luckyyd@tju.edu.cn>
2015-11-03 19:33:59 +0800

    1. 尝试按照Bitcoin的官方Developer Document重构block chain

--- FiatCoinNet.Domain/LowerLevelBlock.cs
+++ FiatCoinNet.Domain/LowerLevelBlock.cs
@@ -11,13 +11,27 @@ namespace FiatCoinNet.Domain
     public class LowerLevelBlock
     {
         [DataMember]
+        public string magicNo = "0xD9B4BEF9";
+
+        [DataMember]
+        public int blockSize { get; set; }
+
+        [DataMember]
+        public BlockHeader blockHeader { get; set; }
+
+        [DataMember]
+        public int TransactionCounter { get; set; }
+
+        [DataMember]
+        public List<PaymentTransaction> TransactionSet { get; set; }
+
+        //Below are not included in bitcoin official doc
+
+        [DataMember]
         public long Period { get; set; }
 
         [DataMember]
         public string Hash { get; set; }
-
-        [DataMember]
-        public List<PaymentTransaction> TransactionSet { get; set; }
 
         [DataMember]
         public string Signature { get; set; }
